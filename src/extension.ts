8412df11b902f2d7bf309cd2547888ffb50f6181
shio-yaamaa <shio.yama520@gmail.com>
2020-03-22 05:33:11 +0900

    Parse and highlight when document changes

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,39 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { ExtensionContext, window, workspace } from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+import Scheduler from './Scheduler';
+import Parser from './Parser';
+import TreeBuilder from './TreeBuilder';
+import Highlighter from './Highlighter';
+import { checkLanguage } from './utility';
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "template-toolkit-tag-pair" is now active!');
+export function activate(context: ExtensionContext) {
+	const scheduler = new Scheduler(100);
+	const parser = new Parser();
+	const treeBuilder = new TreeBuilder();
+	const highlighter = new Highlighter();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	window.onDidChangeActiveTextEditor(editor => {
+		parseAndHighlight(scheduler, parser, treeBuilder, highlighter);
+  }, null, context.subscriptions);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+	workspace.onDidChangeTextDocument(_event => {
+		parseAndHighlight(scheduler, parser, treeBuilder, highlighter);
+	}, null, context.subscriptions);
 }
 
-// this method is called when your extension is deactivated
+const parseAndHighlight = (scheduler: Scheduler, parser: Parser, treeBuilder: TreeBuilder, highlighter: Highlighter) => {
+	const editor = window.activeTextEditor;
+	if (editor && checkLanguage(editor.document)) {
+		scheduler.schedule(() => {
+			try {
+				const tags = parser.parse(editor.document.getText());
+				const tree = treeBuilder.build(tags);
+				highlighter.highlight(editor, tags, tree);
+			} catch (error) {
+				console.error(error.message);
+			}
+		});
+	}
+};
+
 export function deactivate() {}
