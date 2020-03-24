e695dce95245972b1410a1ede8c381f48e1d8627
shio-yaamaa <shio.yama520@gmail.com>
2020-03-25 01:44:35 +0900

    Highlight selected blocks

---
+++ src/SelectionHighlighter.ts
@@ -0,0 +1,35 @@
+import {
+  TextEditor,
+  Selection,
+} from 'vscode';
+
+import { Tag, TagIndex, ParseResult } from './types';
+import { selectionHighlightDecorationType } from './style';
+import { getSelectedTagIndexes, rangeOf } from './utility';
+
+export default class SelectionHighlighter {
+  public highlight(editor: TextEditor, selections: readonly Selection[], parseResult: ParseResult) {
+    const selectedTagIndexes = getSelectedTagIndexes(parseResult.tags, editor.document, selections);
+    const tags = this.getTagsToHighlight(parseResult, selectedTagIndexes);
+    editor.setDecorations(
+      selectionHighlightDecorationType,
+      tags.map(tag => rangeOf(editor.document, tag)),
+    );
+  }
+
+  private getTagsToHighlight(parseResult: ParseResult, selectedTagIndexes: TagIndex[]): Tag[] {
+    const tagIndexes = new Set<TagIndex>();
+    for (const selectedTagIndex of selectedTagIndexes) {
+      const blocks = parseResult.tagToBlocks.get(selectedTagIndex) || [];
+      for (const block of blocks) {
+        tagIndexes.add(block.startTagIndex);
+        for (const middleTagIndex of block.middleTagIndexes) {
+          tagIndexes.add(middleTagIndex);
+        }
+        tagIndexes.add(block.endTagIndex);
+      }
+    }
+    tagIndexes.delete(-1); // Remove -1 that might have been added if a block is incomplete
+    return [...tagIndexes].map(index => parseResult.tags[index]);
+  }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,36 +1,55 @@
 import { ExtensionContext, window, workspace } from 'vscode';
 
+import { ParseResult } from './types';
 import Scheduler from './Scheduler';
 import Parser from './Parser';
 import TreeBuilder from './TreeBuilder';
-import Highlighter from './Highlighter';
-import { checkLanguage } from './utility';
+import DepthHighlighter from './DepthHighlighter';
+import SelectionHighlighter from './SelectionHighlighter';
+import { checkLanguage, mapTagToBlocks } from './utility';
+
+let parseResult: ParseResult | null = null;
 
 export function activate(context: ExtensionContext) {
   const scheduler = new Scheduler(100);
   const parser = new Parser();
   const treeBuilder = new TreeBuilder();
-  const highlighter = new Highlighter();
+  const depthHighlighter = new DepthHighlighter();
+  const selectionHighlighter = new SelectionHighlighter();
 
-  window.onDidChangeActiveTextEditor(editor => {
-    parseAndHighlight(scheduler, parser, treeBuilder, highlighter);
+  window.onDidChangeActiveTextEditor(_editor => {
+    parseAndHighlight(scheduler, parser, treeBuilder, depthHighlighter, selectionHighlighter);
   }, null, context.subscriptions);
 
   workspace.onDidChangeTextDocument(_event => {
-    parseAndHighlight(scheduler, parser, treeBuilder, highlighter);
+    parseAndHighlight(scheduler, parser, treeBuilder, depthHighlighter, selectionHighlighter);
   }, null, context.subscriptions);
+
+  window.onDidChangeTextEditorSelection(event => {
+    if (!parseResult) return;
+    const editor = window.activeTextEditor;
+    if (editor && checkLanguage(editor.document)) {
+      selectionHighlighter.highlight(editor, event.selections, parseResult);
+    }
+  });
+
+  parseAndHighlight(scheduler, parser, treeBuilder, depthHighlighter, selectionHighlighter);
 }
 
-const parseAndHighlight = (scheduler: Scheduler, parser: Parser, treeBuilder: TreeBuilder, highlighter: Highlighter) => {
+const parseAndHighlight = (scheduler: Scheduler, parser: Parser, treeBuilder: TreeBuilder, depthHighlighter: DepthHighlighter, selectionHighlighter: SelectionHighlighter) => {
   const editor = window.activeTextEditor;
   if (editor && checkLanguage(editor.document)) {
     scheduler.schedule(() => {
       try {
         const tags = parser.parse(editor.document.getText());
         const tree = treeBuilder.build(tags);
-        highlighter.highlight(editor, tags, tree);
+        const tagToBlocks = mapTagToBlocks(tree);
+        parseResult = { tags, tree, tagToBlocks };
+        depthHighlighter.highlight(editor, parseResult);
+        selectionHighlighter.highlight(editor, editor.selections, parseResult);
       } catch (error) {
-        console.error(error.message);
+        // Fail silently because parsing errors are expected to occur
+        // when the current template file is work in progress.
       }
     });
   }
